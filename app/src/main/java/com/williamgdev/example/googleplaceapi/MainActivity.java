2c70fdbe0c9ddc14ed0a2ed6ec06410548568ac6
AndroidStorm <williamg.dev@gmail.com>
2018-03-06 16:58:30 -0800

    Add Dependency and Initialize Api

--- app/src/main/java/com/williamgdev/example/googleplaceapi/MainActivity.java
+++ app/src/main/java/com/williamgdev/example/googleplaceapi/MainActivity.java
@@ -1,13 +1,45 @@
 package com.williamgdev.example.googleplaceapi;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GeoDataClient mGeoDataClient;
+    private PlaceDetectionClient mPlaceDetectionClient;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, onConnectionFailListener)
+                .build();
+
     }
+
+    private GoogleApiClient.OnConnectionFailedListener onConnectionFailListener = new GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+            Toast.makeText(MainActivity.this, connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+        }
+    };
 }
